05a0aef1ad0ec5eab5e3bf694d424fbeb55ad93a
Andy McFadden <thefadden@gmail.com>
2015-04-28 15:25:15 -0700

    Exercise runOnUiThread()

--- AudioBypassTest/Assets/Scripts/BypassSoundPlayer.cs
+++ AudioBypassTest/Assets/Scripts/BypassSoundPlayer.cs
@@ -2,15 +2,23 @@
 using System.Collections;
 
 public class BypassSoundPlayer : MonoBehaviour {
+	private AndroidJavaObject activityContext;
 	private AndroidJavaObject audioBypassInstance;
 
 	void Start() {
+		using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+			activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+		}
+
 		using (AndroidJavaClass pluginClass = new AndroidJavaClass("com.faddensoft.androidaudiobypass.AudioBypass")) {
 			Debug.Log("pluginClass is " + pluginClass);
 			audioBypassInstance = pluginClass.CallStatic<AndroidJavaObject>("getInstance");
 		}
 
 		audioBypassInstance.Call("testMethod");
+
+		activityContext.Call("runOnUiThread", new AndroidJavaRunnable(() => {
+			audioBypassInstance.Call("testMethod");
+		}));
 	}
-	
 }
