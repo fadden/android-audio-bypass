bb2cde76119232d6908230f42a29fecf20eec8ee
Andy McFadden <thefadden@gmail.com>
2015-10-17 15:26:09 -0700

    Shift core classes into a separate namespace
    Fought briefly with StreamingAssets.  The Unity editor seems to
    want to un-AudioClip any WAV files in that folder.

--- AudioBypassTest/Assets/Scripts/AndroidAudioBypass/BypassAudioManager.cs
+++ AudioBypassTest/Assets/Scripts/AndroidAudioBypass/BypassAudioManager.cs
@@ -17,39 +17,41 @@ using UnityEngine;
 using System;
 using System.Collections;
 
-/// <summary>
-/// Manages the interaction with the Android SoundPool object.
-/// </summary>
-///
-/// There should only be one of these, but that's not strictly required.
-/// The underlying Java-land code will get upset if it's initialized twice
-/// with different contexts, but since we're passing in the current Activity
-/// that shouldn't happen.
-/// 
-/// This could be made a Unity-object singleton.  I don't want to make it a
-/// C# singleton because the Activity might change with scenes (not sure
-/// about that).
-public class BypassAudioManager : MonoBehaviour {
-    private AndroidJavaObject activityContext;      // android.app.Activity; subclass of android.content.Context
-    private AndroidJavaObject audioBypassInstance;  // com.faddensoft.androidaudiobypass.AudioBypass
+namespace AndroidAudioBypass {
 
     /// <summary>
-    /// Creates an instance of the AudioBypass object, using the Unity JNI bridge.
+    /// Manages the interaction with the Android SoundPool object.
     /// </summary>
-    void Awake() {
-        if (Application.platform != RuntimePlatform.Android) {
-            Debug.LogError("BypassAudioManager only works on Android");
-            throw new SystemException("This is not Android");
-        }
+    ///
+    /// There should only be one of these, but that's not strictly required.
+    /// The underlying Java-land code will get upset if it's initialized twice
+    /// with different contexts, but since we're passing in the current Activity
+    /// that shouldn't happen.
+    ///
+    /// This could be made a Unity-object singleton.  I don't want to make it a
+    /// C# singleton because the Activity might change with scenes (not sure
+    /// about that).
+    public class BypassAudioManager : MonoBehaviour {
+        private AndroidJavaObject activityContext;      // android.app.Activity; subclass of android.content.Context
+        private AndroidJavaObject audioBypassInstance;  // com.faddensoft.androidaudiobypass.AudioBypass
 
-        using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
-            activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
-        }
+        /// <summary>
+        /// Creates an instance of the AudioBypass object, using the Unity JNI bridge.
+        /// </summary>
+        void Awake() {
+            if (Application.platform != RuntimePlatform.Android) {
+                Debug.LogError("BypassAudioManager only works on Android");
+                throw new SystemException("This is not Android");
+            }
 
-        using (AndroidJavaClass pluginClass = new AndroidJavaClass("com.faddensoft.androidaudiobypass.AudioBypass")) {
-            //Debug.Log("pluginClass is " + pluginClass);
-            audioBypassInstance = pluginClass.CallStatic<AndroidJavaObject>("createInstance", activityContext);
-        }
+            using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+                activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+            }
+
+            using (AndroidJavaClass pluginClass = new AndroidJavaClass("com.faddensoft.androidaudiobypass.AudioBypass")) {
+                //Debug.Log("pluginClass is " + pluginClass);
+                audioBypassInstance = pluginClass.CallStatic<AndroidJavaObject>("createInstance", activityContext);
+            }
 
 #if false   // debug stuff
         audioBypassInstance.Call("testMethod");
@@ -60,69 +62,71 @@ public class BypassAudioManager : MonoBehaviour {
 
         Debug.Log("Streaming assets path is '" + Application.streamingAssetsPath + "'");
 #endif
-    }
+        }
 
-    /// <summary>
-    /// Handles object shutdown.  We explicitly shut the SoundPool down to release
-    /// resources without having to go through various garbage collection passes.
-    /// </summary>
-    void OnDestroy() {
-        if (audioBypassInstance != null) {
-            Debug.Log("Shutting down SoundPool");
-            audioBypassInstance.Call("destroy");
-            audioBypassInstance = null;
+        /// <summary>
+        /// Handles object shutdown.  We explicitly shut the SoundPool down to release
+        /// resources without having to go through various garbage collection passes.
+        /// </summary>
+        void OnDestroy() {
+            if (audioBypassInstance != null) {
+                Debug.Log("Shutting down SoundPool");
+                audioBypassInstance.Call("destroy");
+                audioBypassInstance = null;
+            }
+        }
+
+        /// <summary>
+        /// Registers a sound file with the SoundPool.
+        /// </summary>
+        /// <returns>The sound ID to use on subsequent play() calls.</returns>
+        /// <param name="soundFile">File name in StreamingAssets directory.</param>
+        public int RegisterSoundFile(string soundFile) {
+            if (audioBypassInstance == null) { return -1; }
+
+            // TODO: if sound file is already registered, don't register it again.
+            // Note that this potentially makes UnregisterSound() more complicated
+            // as we would need to refcount.
+
+            int soundId = audioBypassInstance.Call<int>("register", soundFile);
+            Debug.Log("Registered " + soundFile + " as ID " + soundId);
+
+            // TODO: the sound isn't necessarily ready to play at this point -- the
+            // load mechanism is asynchronous.  We would need to tie into the
+            // SoundPool OnLoadCompleteListener to figure out when sounds were
+            // actually ready to go.
+
+            return soundId;
+        }
+
+        public void UnregisterSound(int soundId) {
+            // TODO -- should call through to SoundPool.unload(soundId)
+        }
+
+        /// <summary>
+        /// Plays the specified sound.  Parameters are passed through to SoundPool.play().
+        /// </summary>
+        /// <param name="soundId">Sound identifier, returned earlier by the registration method.</param>
+        /// <param name="leftVolume">Left volume, [0.0, 1.0].</param>
+        /// <param name="rightVolume">Right volume, [0.0, 1.0].</param>
+        /// <param name="priority">Priority (0 = lowest).</param>
+        /// <param name="loop">Loop mode (0 = no loop, -1 = loop forever).</param>
+        /// <param name="rate">Rate (1.0 = normal playback, [0.5, 2.0]).</param>
+        public void PlaySound(int soundId, float leftVolume, float rightVolume, int priority, int loop, float rate) {
+            if (audioBypassInstance == null) { return; }
+
+            // It shouldn't matter which thread we call this from, so no need to
+            // route through runOnUiThread().
+            int streamId = audioBypassInstance.Call<int>("play", soundId, leftVolume, rightVolume,
+                                                         priority, loop, rate);
+            if (streamId == -1) {
+                Debug.LogWarning("PlaySound id=" + soundId + " failed");
+
+                // TODO: this is useful for sounds that are looping (so you
+                // can stop them) or just very long, like music.  We should
+                // return it and provide the relevant functions.
+            }
         }
     }
 
-    /// <summary>
-    /// Registers a sound file with the SoundPool.
-    /// </summary>
-    /// <returns>The sound ID to use on subsequent play() calls.</returns>
-    /// <param name="soundFile">File name in StreamingAssets directory.</param>
-    public int RegisterSoundFile(string soundFile) {
-        if (audioBypassInstance == null) { return -1; }
-
-        // TODO: if sound file is already registered, don't register it again.
-        // Note that this potentially makes UnregisterSound() more complicated
-        // as we would need to refcount.
-
-        int soundId = audioBypassInstance.Call<int>("register", soundFile);
-        Debug.Log("Registered " + soundFile + " as ID " + soundId);
-
-        // TODO: the sound isn't necessarily ready to play at this point -- the
-        // load mechanism is asynchronous.  We would need to tie into the
-        // SoundPool OnLoadCompleteListener to figure out when sounds were
-        // actually ready to go.
-
-        return soundId;
-    }
-
-    public void UnregisterSound(int soundId) {
-        // TODO -- should call through to SoundPool.unload(soundId)
-    }
-
-    /// <summary>
-    /// Plays the specified sound.  Parameters are passed through to SoundPool.play().
-    /// </summary>
-    /// <param name="soundId">Sound identifier, returned earlier by the registration method.</param>
-    /// <param name="leftVolume">Left volume, [0.0, 1.0].</param>
-    /// <param name="rightVolume">Right volume, [0.0, 1.0].</param>
-    /// <param name="priority">Priority (0 = lowest).</param>
-    /// <param name="loop">Loop mode (0 = no loop, -1 = loop forever).</param>
-    /// <param name="rate">Rate (1.0 = normal playback, [0.5, 2.0]).</param>
-    public void PlaySound(int soundId, float leftVolume, float rightVolume, int priority, int loop, float rate) {
-        if (audioBypassInstance == null) { return; }
-
-        // It shouldn't matter which thread we call this from, so no need to
-        // route through runOnUiThread().
-        int streamId = audioBypassInstance.Call<int>("play", soundId, leftVolume, rightVolume,
-                                                     priority, loop, rate);
-        if (streamId == -1) {
-            Debug.LogWarning("PlaySound id=" + soundId + " failed");
-
-            // TODO: this is useful for sounds that are looping (so you
-            // can stop them) or just very long, like music.  We should
-            // return it and provide the relevant functions.
-        }
-    }
-}
+} // namespace AndroidAudioBypass
--- AudioBypassTest/Assets/Scripts/AndroidAudioBypass/BypassAudioSource.cs
+++ AudioBypassTest/Assets/Scripts/AndroidAudioBypass/BypassAudioSource.cs
@@ -17,41 +17,52 @@ using UnityEngine;
 using System;
 using System.Collections;
 
-/// <summary>
-/// AudioSource equivalent for the Android bypass mechanism.
-/// </summary>
-public class BypassAudioSource : MonoBehaviour {
-    public BypassAudioManager m_bypassAudioManager;
-    public string m_audioFile;
-    public int m_priority = 1;          // stream priority (0 = lowest)
-    public float m_volume = 1.0f;       // volume [0.0, 1.0]
-    public float m_rate = 1.0f;         // playback rate (1.0 = normal; [0.5, 2.0])
-    public bool m_playOnAwake = false;
-    public bool m_loop = false;
+namespace AndroidAudioBypass {
 
-    private int m_soundId;
+    /// <summary>
+    /// AudioSource equivalent for the Android bypass mechanism.
+    /// </summary>
+    public class BypassAudioSource : MonoBehaviour {
+        public BypassAudioManager m_bypassAudioManager;
+        public string m_audioFile;
 
-    public void Start() {
-        if (String.IsNullOrEmpty(m_audioFile)) {
-            Debug.LogError("Audio file not specified in " + gameObject.name);
-            return;
+        [Range(0.0f, 1.0f)]
+        public int m_priority = 1;          // stream priority (0 = lowest)
+
+        [Range(0.0f, 1.0f)]
+        public float m_volume = 1.0f;       // volume [0.0, 1.0]
+
+        [Range(0.5f, 2.0f)]
+        public float m_rate = 1.0f;         // playback rate (1.0 = normal; [0.5, 2.0])
+
+        public bool m_playOnAwake = false;
+        public bool m_loop = false;
+
+        private int m_soundId;
+
+        public void Start() {
+            if (String.IsNullOrEmpty(m_audioFile)) {
+                Debug.LogError("Audio file not specified in " + gameObject.name);
+                return;
+            }
+            m_soundId = m_bypassAudioManager.RegisterSoundFile(m_audioFile);
+
+            if (m_playOnAwake) {
+                Play();
+            }
         }
-        m_soundId = m_bypassAudioManager.RegisterSoundFile(m_audioFile);
 
-        if (m_playOnAwake) {
-            Play();
+        public void Play() {
+            if (m_rate < 0.5f) {
+                m_rate = 0.5f;
+            }
+            if (m_rate > 2.0f) {
+                m_rate = 2.0f;
+            }
+
+            m_bypassAudioManager.PlaySound(m_soundId, m_volume, m_volume, m_priority,
+                                           m_loop ? -1 : 0, m_rate);
         }
     }
 
-    public void Play() {
-        if (m_rate < 0.5f) {
-            m_rate = 0.5f;
-        }
-        if (m_rate > 2.0f) {
-            m_rate = 2.0f;
-        }
-
-        m_bypassAudioManager.PlaySound(m_soundId, m_volume, m_volume, m_priority,
-                                       m_loop ? -1 : 0, m_rate);
-    }
-}
+} // namespace AndroidAudioBypass
